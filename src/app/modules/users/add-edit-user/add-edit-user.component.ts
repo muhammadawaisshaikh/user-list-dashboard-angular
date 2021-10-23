ec018802f2194b7e801219054ab94c66e2187833
Muhammad Awais <muhammad.awais@royalcyber.com>
2021-10-23 21:30:44 +0500

    user creation with validations and reflected in DB json success

--- src/app/modules/users/add-edit-user/add-edit-user.component.ts
+++ src/app/modules/users/add-edit-user/add-edit-user.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UsersService } from 'src/app/core/http/services/users/users.service';
 
 @Component({
   selector: 'app-add-edit-user',
@@ -11,7 +13,9 @@ export class AddEditUserComponent implements OnInit {
   programForm: any = FormGroup;
 
   constructor(
+    private router: Router,
     private fb: FormBuilder,
+    private usersService: UsersService
   ) { }
 
   ngOnInit(): void {
@@ -20,13 +24,14 @@ export class AddEditUserComponent implements OnInit {
 
   formInit() {
     this.programForm = this.fb.group({
-      firstName: ['', [Validators.required]],
-      lastName: ['', [Validators.required]],
-      email: ['', [Validators.required]],
-      personalId: ['', [Validators.required]],
-      profilePhoto: ['', [Validators.required]],
+      id: [this.randomId()],
+      firstName: ['', [Validators.required, Validators.maxLength(60)]],
+      lastName: ['', [Validators.required, Validators.maxLength(60)]],
+      email: ['', [Validators.required, Validators.email]],
+      personalId: ['', [Validators.required, Validators.minLength(11), Validators.maxLength(11)]],
+      profilePhoto: ['https://i.ibb.co/2MH630J/user.png', [Validators.required]],
       mobileNumber: ['', [Validators.required]],
-      gender: ['', [Validators.required]],
+      gender: ['male', [Validators.required]],
       street: ['', [Validators.required]],
       city: ['', [Validators.required]],
       country: ['', [Validators.required]],
@@ -34,9 +39,20 @@ export class AddEditUserComponent implements OnInit {
     })
   }
 
+  randomId = () => {
+    return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+  }
+
   onSave(programForm: any) {
     let data = programForm.value;
-    console.log(data);
+    
+    this.usersService.addUser(data).subscribe((res: any) => {
+      console.log(res);
+      alert(res.firstName + " - New User Created")
+    },(error) => {
+      alert('User Creation Failed!');
+      this.router.navigateByUrl('/users/user-listing');
+    })
   }
 
   onUpdate(programForm: any) {
